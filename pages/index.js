3a1b97ea37e73e101998c12590438be96b67f9f7
Alena Batushkova <alena.batushkova@gmail.com>
2020-04-11 18:16:40 +0300

    add comments to js, change open-close func

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,46 @@
 const popupWindow = document.querySelector('.popup');
-const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonEdit = document.querySelector('.profile__button_type_edit');
 const buttonClose = document.querySelector('.popup__close-button');
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__job');
-const nameInput = document.querySelector('#input-name');
-const jobInput = document.querySelector('#input-job');
+const nameInput = document.querySelector('.popup__input_type_profile-name');
+const jobInput = document.querySelector('.popup__input_type_profile-job');
+const formElement = document.querySelector('.popup__form');
 
 function setInputValues(name, job) {
   nameInput.value = name;
   jobInput.value = job;
 }
 
-function openPopup() {
-  popupWindow.classList.add('popup_opened');
+function togglePopup() {
+  if (!popupWindow.classList.contains('popup_opened')) {
+    popupWindow.classList.add('popup_opened');
 
-  setInputValues(nameProfile.textContent, jobProfile.textContent);
+    setInputValues(nameProfile.textContent, jobProfile.textContent);
+  } else {
+    popupWindow.classList.remove('popup_opened');
+  }
 }
-
-function closePopup() {
-  popupWindow.classList.remove('popup_opened');
-}
-
-buttonEdit.addEventListener('click', openPopup);
-buttonClose.addEventListener('click', closePopup);
-
-const formElement = document.querySelector('#popup-form');
 
 function formSubmitHandler (evt) {
-  evt.preventDefault();
+  evt.preventDefault(); // prevent default action of submit
   
+  // set new profile name if it's different 
   if (nameProfile.textContent !== nameInput.value) {
     nameProfile.textContent = nameInput.value;
+
+    // set profile name as avatar alt
     document.querySelector('.profile__avatar').setAttribute('alt', `${nameInput.value}.`);
   }
   
+  // set new profile job if it's different
   if (jobProfile.textContent !== jobInput.value) {
     jobProfile.textContent = jobInput.value;
   }
-
-  closePopup();
+  
+  togglePopup();
 }
 
+buttonEdit.addEventListener('click', togglePopup);
+buttonClose.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', formSubmitHandler);
